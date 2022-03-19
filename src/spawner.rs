5d8ddf0655b7f52eee5e9c56b17cdbdb349c8d47
TamplierS1 <azamat.nabiullin.97@mail.ru>
2022-03-19 10:23:24 +0300

    Added health

--- src/spawner.rs
+++ src/spawner.rs
@@ -8,22 +8,44 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             color: ColorPair::new(DARK_GREY, BLACK),
             glyph: to_cp437('@'),
         },
+        Health {
+            current: 20,
+            max: 20,
+        },
     ));
 }
 
 pub fn spawn_monster(ecs: &mut World, pos: Point) {
+    let enemy_type = RandomNumberGenerator::new().range::<i32>(0, 4);
+    let glyph = match enemy_type {
+        0 => to_cp437('E'),
+        1 => to_cp437('O'),
+        2 => to_cp437('o'),
+        _ => to_cp437('g'),
+    };
+
     ecs.push((
         Enemy,
         pos,
         Render {
             color: ColorPair::new(DARKKHAKI, BLACK),
-            glyph: match RandomNumberGenerator::new().range(0, 4) {
-                0 => to_cp437('E'),
-                1 => to_cp437('O'),
-                2 => to_cp437('o'),
-                _ => to_cp437('g'),
-            },
+            glyph,
         },
         MovingRandomly,
+        match enemy_type {
+            0 => Health {
+                current: 30,
+                max: 30,
+            },
+            1 => Health {
+                current: 17,
+                max: 17,
+            },
+            2 => Health {
+                current: 10,
+                max: 10,
+            },
+            _ => Health { current: 6, max: 6 },
+        },
     ));
 }
